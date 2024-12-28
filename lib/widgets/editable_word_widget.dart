9bb4aa038043d0886b758b92b6d18b6792197ba5
nicodevelop <nicolas@nico-develop.com>
2024-12-28 10:49:20 +0100

    Ajout de la gestion du focus et du survol dans le widget EditableWord, amélioration de l'interface utilisateur avec des bordures dynamiques et mise à jour des styles de texte.

--- lib/widgets/editable_word_widget.dart
+++ lib/widgets/editable_word_widget.dart
@@ -1,3 +1,4 @@
+import 'package:com_nicodevelop_xmagicmovie/constants.dart';
 import 'package:com_nicodevelop_xmagicmovie/models/transcription/word_model.dart';
 import 'package:flutter/material.dart';
 
@@ -15,38 +16,68 @@ class EditableWordWidget extends StatefulWidget {
 
 class _EditableWordWidgetState extends State<EditableWordWidget> {
   late TextEditingController _controller;
+  late FocusNode _focusNode;
+  bool _isHovering = false;
 
   @override
   void initState() {
     super.initState();
     _controller = TextEditingController(text: widget.wordModel.word);
+    _focusNode = FocusNode();
+
+    _focusNode.addListener(() {
+      if (!_focusNode.hasFocus) {
+        setState(() {
+          _isHovering = false;
+        });
+      }
+    });
   }
 
   @override
   void dispose() {
     _controller.dispose();
+    _focusNode.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey),
-        borderRadius: BorderRadius.circular(8.0),
-      ),
-      child: IntrinsicWidth(
-        child: TextField(
-          controller: _controller,
-          decoration: const InputDecoration(
-            border: InputBorder.none,
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      onEnter: (_) => setState(() => _isHovering = true),
+      onExit: (_) => setState(() => _isHovering = false),
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: kDefaultPadding,
+          vertical: kDefaultPadding / 2,
+        ),
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: _isHovering || _focusNode.hasFocus
+                ? Colors.grey
+                : Colors.transparent,
           ),
-          onSubmitted: (newValue) {
-            setState(() {
-              // widget.wordModel.word = newValue; // Met à jour le modèle
-            });
-          },
+          borderRadius: BorderRadius.circular(
+            kDefaultPadding / 2,
+          ),
+        ),
+        child: IntrinsicWidth(
+          child: TextField(
+            controller: _controller,
+            focusNode: _focusNode,
+            decoration: const InputDecoration(
+              isDense: true,
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.zero,
+            ),
+            style: const TextStyle(
+              fontSize: kDefaultPadding * 1.8,
+            ),
+            onSubmitted: (newValue) {
+              setState(() {});
+            },
+          ),
         ),
       ),
     );
